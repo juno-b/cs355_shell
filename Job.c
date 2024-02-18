923c7602b0189ff8b336e90d2d801c9aa352f6e8
El Bartsch <jbartsch@haverford.edu>
2024-02-18 14:38:35 -0500

    job updates

--- Job.c
+++ Job.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include "Job.h"
 
-struct Job *newJob(pid_t id) { // Creates a new node with item
+struct Job *newJob(pid_t id, char *com, int stat) { // Creates a new node with item
     struct Job *n = malloc(sizeof(struct Job));
     pid_t pid = id;
-    //char *command = command;
-    //int job_number;
-    //int status; // 0: running, 1: stopped, 2: done
+    char *command = com;
+    int status = stat; // 0: running, 1: stopped, 2: done
     struct Job *next = NULL;
 } // newNode()
--- Job.h
+++ Job.h
@@ -3,12 +3,11 @@
 
 typedef struct Job {
     pid_t pid;
-    //char *command;
-    //int job_number;
-    //int status; // 0: running, 1: stopped, 2: done
+    char *command;
+    int status; // 0: running, 1: stopped, 2: done
     struct Job *next;
 } Job;
 
-struct Job *newJob(pid_t id);//, char *command, int job_number, int status);
+struct Job *newJob(pid_t id, char *com, int stat);
 
 #endif
